becd1e2dea3a1640cb64a50df4371c0f92199425
rtrepiana <rtrepiana@aonsolutions.es>
2017-09-12 13:28:51 +0200

    Adds eslint, travis file ...

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 'use strict';
 
 module.exports = {
-  domain : require('./lib/domain'),
-  agreement : require('./lib/agreement'),
-  invoice : require ('./lib/invoice/invoice'),
-  sabbatic : require ('./lib/invoice/sabbatic'),
-  settlement : require('./lib/settlement'),
+	domain : require('./lib/domain'),
+	agreement : require('./lib/agreement'),
+	invoice : require ('./lib/invoice/invoice'),
+	sabbatic : require ('./lib/invoice/sabbatic'),
+	settlement : require('./lib/settlement'),
 };

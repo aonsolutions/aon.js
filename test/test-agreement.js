b7669072e660f33008a00845ebbe8e3be67aa030
Sergio Valdepeñas <sergio@MacBook-Pro-de-Sergio.local>
2017-09-25 12:56:46 +0200

    Probado modificar informacion existente, SET Agreement

--- test/test-agreement.js
+++ test/test-agreement.js
@@ -28,7 +28,11 @@ aon.agreement.get(pool,
   function(params){ return params.id.equals(1156)},
   function(pool, agreement){
 
-    modificarAgreement(agreement);
+    // modificarAgreement(agreement); //modifica description
+    // modificarPayments(agreement); //modifica payments
+    // modificarExtras(agreement); //modifica extras
+    // modificarLevelCategory(agreement); //modifica levelCategory
+    // modificarPeriods(agreement); //modifica Periods
 
     aon.agreement.set(pool,
       agreement,
@@ -45,5 +49,35 @@ aon.agreement.get(pool,
 
 modificarAgreement = function(agreement){
   agreement.description = "CONVENIO MADRID";
-  //agreement.description = "CONVENIO COLECTIVO DE OFICINAS Y DESPACHOS PARA MADRID";
+}
+
+modificarPayments = function(agreement){
+  var payments = agreement.payments;
+  payments[0].irpfExpression = "Caracola";
+}
+
+modificarExtras = function(agreement){
+  var extras = agreement.extras;
+  extras[0].start_date = "6/7";
+  extras[0].end_date = "1/12"
+  extras[0].issue_date = "66/66"
+  var agreePay = extras[1].agreePayment;
+  agreePay.quoteExpression = "Caracola";
+}
+
+modificarLevelCategory = function(agreement){
+  var levelsCategory = agreement.levelsCategory;
+  levelsCategory[0].description = "001";
+  var categories = levelsCategory[1].categories;
+  categories[0].description = "PERSONAL TITULADO URJC";
+}
+
+modificarPeriods = function(agreement){
+  var period = agreement.periods[0];
+  var newDate = new Date(2010,05,13);
+  period.start_date = newDate;
+  var levels = period.levels;
+  levels[0].datas.SALARIO_ANUAL = "66666";
+  levels[11].datas.JORNADA_LABORAL = "66666";
+  levels[11].datas.IMPORTE_KM = "0.66666";
 }

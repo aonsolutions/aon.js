80b8f2dc0d3089bc58abed1c37fd54403880e62c
Sergio Valdepeñas <sergio@MacBook-Pro-de-Sergio.local>
2017-09-26 13:09:21 +0200

    Probado insertar y borrar payments y extras. Falta insertar y borrar periodos/payments y level/level_category. SET Agreement

--- test/test-agreement.js
+++ test/test-agreement.js
@@ -33,17 +33,23 @@ aon.agreement.get(pool,
     // modificarExtras(agreement); //modifica extras
     // modificarLevelCategory(agreement); //modifica levelCategory
     // modificarPeriods(agreement); //modifica Periods
+    // insertarPayment(agreement); //insertar payments
+    // borrarPayment(agreement); //borrar payments
+    // insertarExtra(agreement); //insertar extra
+    // borrarExtra(agreement); //borrar extra
+    // //insertarAgreeLevelCat(agreement); //insertar Agreement Level Category
+    // borrarAgreeLevelCat(agreement); //borrar Agreement Level Category
+    //
+    // aon.agreement.set(pool,
+    //   agreement,
+    //   function(pool, agreement){
+    //     //process.stdout.write(JSON.stringify(agreement));
+    //     //process.exit();
+    //   }
+    // );
 
-    aon.agreement.set(pool,
-      agreement,
-      function(pool, agreement){
-        //process.stdout.write(JSON.stringify(agreement));
-        //process.exit();
-      }
-    );
-
-    // process.stdout.write(JSON.stringify(agreement));
-    // process.exit();
+    process.stdout.write(JSON.stringify(agreement));
+    process.exit();
   }
 );
 
@@ -80,4 +86,74 @@ modificarPeriods = function(agreement){
   levels[0].datas.SALARIO_ANUAL = "66666";
   levels[11].datas.JORNADA_LABORAL = "66666";
   levels[11].datas.IMPORTE_KM = "0.66666";
+}
+
+insertarPayment = function(agreement){
+  var payments = agreement.payments;
+  var newPay = {
+    id: null,
+    code: "SALARIO MIERDA",
+    expression: "/*user*/SALARIO_ANUAL/**/ / NO_PAGAS * DIAS_TRABAJADOS / DIAS ",
+    description: "[1]SALARIO MIERDA",
+    type: "CRA 2",
+    irpfExpression: "__W",
+    quoteExpression: "__W"
+  }
+  payments.push(newPay);
+}
+
+borrarPayment = function(agreement){
+  var payments = agreement.payments;
+  payments.splice(0,1);
+}
+
+insertarExtra = function(agreement){
+  var extras = agreement.extras;
+  var newPay = {
+    id: null,
+    expression: "/*user*/SALARIO_ANUAL/**/ / NO_PAGAS * DIAS_TRABAJADOS / DIAS ",
+    description: "[1]SALARIO MIERDA",
+    type: "CRA 2",
+    irpfExpression: "__W",
+    quoteExpression: "__W"
+  }
+  var newExtra = {
+    start_date: "01/01",
+    end_date: "31/12",
+    issue_date: "06/06",
+    agreePayment: newPay
+  }
+  extras.push(newExtra);
+}
+
+borrarExtra = function(agreement){
+  var extras = agreement.extras;
+  extras.splice(0,1);
+}
+
+//ESTE METODO NO FUNCIONA AUN
+insertarAgreeLevelCat = function(agreement){
+  var levelsCat = agreement.levelsCategory;
+  var newCategories = [];
+  var newCat = {
+    id: null,
+    description: "PEON SUMISO"
+  }
+  var newCat2 = {
+    id: null,
+    description: "NERD"
+  }
+  newCategories.push(newCat);
+  newCategories.push(newCat2);
+  var newLevelCat = {
+    id: null,
+    description: "NIVEL 66",
+    categories: newCategories
+  }
+  levelsCat.push(newLevelCat);
+}
+
+borrarAgreeLevelCat = function(agreement){
+  var levelsCat = agreement.levelsCategory;
+  levelsCat.splice(0,1);
 }

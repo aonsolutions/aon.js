e2670945cee754a683d26c76a365381490d54888
aibanez <aibanez@aonsolutions.es>
2017-10-18 12:53:36 +0200

    Invoice import from file.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ module.exports = {
 	domain : require('./lib/domain'),
 	agreement : require('./lib/agreement'),
 	invoice : require ('./lib/invoice/invoice'),
-	sabbatic : require ('./lib/invoice/sabbatic'),
+	invoiceImport : require ('./lib/invoice/invoiceImport'),
+	sabbatic : require ('./lib/invoice/sabbaticOld'),
 	settlement : require('./lib/settlement'),
 	auth : require ('./lib/auth/auth'),
 	file : require ('./lib/file')

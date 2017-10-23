5c624438c5f7cc320699b233dcac211cc0b92f7e
rtrepiana <rtrepiana@aonsolutions.es>
2017-10-23 09:16:36 +0200

    Clean <<<<

--- index.js
+++ index.js
@@ -2,16 +2,6 @@
 'use strict';
 
 module.exports = {
-<<<<<<< Updated upstream
-	domain : require('./lib/domain'),
-	agreement : require('./lib/agreement'),
-	invoice : require ('./lib/invoice/invoice'),
-	invoiceImport : require ('./lib/invoice/invoiceImport'),
-	//sabbatic : require ('./lib/invoice/sabbaticOld'),
-	settlement : require('./lib/settlement'),
-	//auth : require ('./lib/auth/auth'),
-	file : require ('./lib/file')
-=======
 	domain : require('./lib/domain')
 	//, agreement : require('./lib/agreement')
 	//, invoice : require ('./lib/invoice/invoice')
@@ -19,5 +9,4 @@ module.exports = {
 	, settlement : require('./lib/settlement')
 	//, auth : require ('./lib/auth/auth')
 	//, file : require ('./lib/file')
->>>>>>> Stashed changes
 };

d8d4cf362019bb21d53bd8c2782ffd87805926ab
aibanez <aibanez@aonsolutions.es>
2017-11-23 15:02:26 +0100

    invoice import.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = {
 	domain : require('./lib/domain')
 	//, agreement : require('./lib/agreement')
-	, invoice : require ('./lib/invoice/invoiceImport')
+	, invoice : require ('./lib/invoice/invoiceImportDynamo')
 	, settlement : require('./lib/settlement')
 	//, auth : require ('./lib/auth/auth')
 	//, file : require ('./lib/file')
